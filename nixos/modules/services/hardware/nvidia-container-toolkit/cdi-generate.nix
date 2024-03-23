fa916620e8496e98118c0df3d636582be333a591
Rafael Fernández López <ereslibre@ereslibre.es>
2024-03-23 17:05:01 +0100

    wip

--- nixos/modules/services/hardware/nvidia-container-toolkit/cdi-generate.nix
+++ nixos/modules/services/hardware/nvidia-container-toolkit/cdi-generate.nix
@@ -3,56 +3,30 @@
   glibc,
   jq,
   lib,
+  extra-mounts,
   nvidia-container-toolkit,
   nvidia-driver,
   runtimeShell,
   writeScriptBin,
-  mount-nvidia-binaries,
-  mount-nvidia-docker-1-directories,
-}:
-let
-  mountOptions = { options = ["ro" "nosuid" "nodev" "bind"]; };
+}: let
+  mkMount = {hostPath, containerPath, mountOptions ? null}: {
+    inherit hostPath containerPath;
+    options = (if mountOptions != null then mountOptions else [ "ro" "nosuid" "nodev" "bind" ]);
+  };
+  jqAddMountExpression = ".containerEdits.mounts[.containerEdits.mounts | length] |= . +";
   mounts = [
-    # FIXME: use closureinfo
-    {
-      hostPath = addDriverRunpath.driverLink;
-      containerPath = addDriverRunpath.driverLink;
-    }
+    { hostPath = addDriverRunpath.driverLink;
+      containerPath = addDriverRunpath.driverLink; }
     { hostPath = "${lib.getLib glibc}/lib";
       containerPath = "${lib.getLib glibc}/lib"; }
     { hostPath = "${lib.getLib glibc}/lib64";
       containerPath = "${lib.getLib glibc}/lib64"; }
-  ] ++ (
-    lib.optionals mount-nvidia-binaries [
-      { hostPath = lib.getExe' nvidia-driver "nvidia-smi";
-        containerPath = "/usr/bin/nvidia-smi"; }
-      { hostPath = lib.getExe' nvidia-driver "nvidia-cuda-mps-control";
-        containerPath = "/usr/bin/nvidia-cuda-mps-control"; }
-      { hostPath = lib.getExe' nvidia-driver "nvidia-cuda-mps-server";
-        containerPath = "/usr/bin/nvidia-cuda-mps-server"; }
-      { hostPath = lib.getExe' nvidia-driver "nvidia-debugdump";
-        containerPath = "/usr/bin/nvidia-debugdump"; }
-      { hostPath = lib.getExe' nvidia-driver "nvidia-powerd";
-        containerPath = "/usr/bin/nvidia-powerd"; }
-      { hostPath = lib.getExe' nvidia-container-toolkit "nvidia-ctk";
-        containerPath = "/usr/bin/nvidia-ctk"; }
-    ]
-  ) ++ (
-    # nvidia-docker 1.0 uses /usr/local/nvidia/lib{,64}
-    #   e.g.
-    #     - https://gitlab.com/nvidia/container-images/cuda/-/blob/e3ff10eab3a1424fe394899df0e0f8ca5a410f0f/dist/12.3.1/ubi9/base/Dockerfile#L44
-    #     - https://github.com/NVIDIA/nvidia-docker/blob/01d2c9436620d7dde4672e414698afe6da4a282f/src/nvidia/volumes.go#L104-L173
-    lib.optionals mount-nvidia-docker-1-directories [
-      { hostPath = "${lib.getLib nvidia-driver}/lib";
-        containerPath = "/usr/local/nvidia/lib"; }
-      { hostPath = "${lib.getLib nvidia-driver}/lib";
-        containerPath = "/usr/local/nvidia/lib64"; }
-    ]);
-  jqAddMountExpression = ".containerEdits.mounts[.containerEdits.mounts | length] |= . +";
-  mountsToJq = lib.concatMap
+  ];
+  mountsToJq = mounts: (lib.concatMap
     (mount:
-      ["${lib.getExe jq} '${jqAddMountExpression} ${builtins.toJSON (mount // mountOptions)}'"])
-    mounts;
+      ["${lib.getExe jq} '${jqAddMountExpression} ${builtins.toJSON (mkMount mount)}'"])
+    mounts);
+  allMounts = (mountsToJq mounts) ++ (mountsToJq extra-mounts);
 in
 writeScriptBin "nvidia-cdi-generator"
 ''
@@ -67,5 +41,5 @@ function cdiGenerate {
 }
 
 cdiGenerate | \
-  ${lib.concatStringsSep " | " mountsToJq} > $RUNTIME_DIRECTORY/nvidia-container-toolkit.json
+  ${lib.concatStringsSep " | " allMounts} > $RUNTIME_DIRECTORY/nvidia-container-toolkit.json
 ''
--- nixos/modules/services/hardware/nvidia-container-toolkit/default.nix
+++ nixos/modules/services/hardware/nvidia-container-toolkit/default.nix
@@ -2,32 +2,58 @@
 
 {
 
-  options = {
+  options = let
+    mountType = {
+      options = {
+        hostPath = lib.mkOption {
+          type = lib.types.str;
+          description = lib.mdDoc "Host path.";
+        };
+        containerPath = lib.mkOption {
+          type = lib.types.str;
+          description = lib.mdDoc "Container path.";
+        };
+        mountOptions = lib.mkOption {
+          default = [ "ro" "nosuid" "nodev" "bind" ];
+          type = lib.types.listOf lib.types.str;
+          description = lib.mdDoc "Mount options.";
+        };
+      };
+    };
+  in {
 
     hardware.nvidia-container-toolkit = {
       enable = lib.mkOption {
         default = false;
         type = lib.types.bool;
         description = lib.mdDoc ''
-        Enable dynamic CDI configuration for NVidia devices by running
-        nvidia-container-toolkit on boot.
-      '';
+          Enable dynamic CDI configuration for NVidia devices by running
+          nvidia-container-toolkit on boot.
+        '';
       };
 
-      mount-nvidia-binaries = lib.mkOption {
+      extra-mounts = lib.mkOption {
+        type = lib.types.listOf (lib.types.submodule mountType);
+        default = [];
+        description = lib.mdDoc "Extra mounts to be added to every container under this CDI profile.";
+      };
+
+      mount-nvidia-executables = lib.mkOption {
         default = true;
         type = lib.types.bool;
         description = lib.mdDoc ''
-            Mount binaries nvidia-smi, nvidia-cuda-mps-control, nvidia-cuda-mps-server, nvidia-debugdump, nvidia-powerd and nvidia-ctk on containers.
-          '';
+          Mount executables nvidia-smi, nvidia-cuda-mps-control, nvidia-cuda-mps-server,
+          nvidia-debugdump, nvidia-powerd and nvidia-ctk on containers.
+        '';
       };
 
       mount-nvidia-docker-1-directories = lib.mkOption {
         default = true;
         type = lib.types.bool;
         description = lib.mdDoc ''
-            Mount nvidia-docker-1 directories on containers: /usr/local/nvidia/lib and /usr/local/nvidia/lib64.
-          '';
+          Mount nvidia-docker-1 directories on containers: /usr/local/nvidia/lib and
+          /usr/local/nvidia/lib64.
+        '';
       };
 
     };
@@ -46,9 +72,7 @@
         ExecStart =
           let
             script = pkgs.callPackage ./cdi-generate.nix {
-              inherit (config.hardware.nvidia-container-toolkit)
-                mount-nvidia-binaries
-                mount-nvidia-docker-1-directories;
+              inherit (config.hardware.nvidia-container-toolkit) extra-mounts;
               nvidia-driver = config.hardware.nvidia.package;
             };
           in
